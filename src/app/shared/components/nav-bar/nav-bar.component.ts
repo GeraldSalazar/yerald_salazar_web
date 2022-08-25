a597caabf8ee43a29148b2c393f069ef91e796e3
GeraldSalazar <geraldsalazar4@gmail.com>
2022-08-25 13:57:37 -0600

    navigation bar view (without functionality implemented)

---
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'],
+})
+export class NavBarComponent implements OnInit {
+  constructor(private breakPointsObs: BreakpointObserver) {}
+  shiftMenu: boolean = false; //To show the mobile menu and hide nav
+  showMobileMenu: boolean = false; //To show the nav menu when mobile view
+
+  ngOnInit() {
+    this.checkMenuBreakPoint();
+  }
+
+  toggleBarsMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
+
+  checkMenuBreakPoint() {
+    this.breakPointsObs.observe([Breakpoints.XSmall]).subscribe(result => {
+      this.showMobileMenu = false;
+      if (result.matches) {
+        this.shiftMenu = true;
+      } else {
+        this.shiftMenu = false;
+      }
+    });
+  }
+}
