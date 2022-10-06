2ef8f1c6bc314f52b9296031b65f117b6acdaf5b
GeraldSalazar <geraldsalazar4@gmail.com>
2022-10-05 19:43:16 -0600

    added basic form structure

--- src/app/modules/contact/form/form.component.ts
+++ src/app/modules/contact/form/form.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent implements OnInit {
-  firstName = new FormControl('');
+export class FormComponent {
+  contactForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    message: new FormControl(''),
+  });
   constructor() {}
-
-  ngOnInit(): void {}
 }
