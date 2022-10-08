573ed5ade3d9c41cb4d3438ee06c88c4568cc588
GeraldSalazar <geraldsalazar4@gmail.com>
2022-10-08 11:01:28 -0600

    skills & concepts added to about me page

--- src/app/modules/about-me/about-me.module.ts
+++ src/app/modules/about-me/about-me.module.ts
@@ -5,9 +5,10 @@ import { AboutMeRoutingModule } from './about-me-routing.module';
 import { AboutMeComponent } from './about-me.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CvDownloadComponent } from './components/cv-download/cv-download.component';
+import { SkillsConceptsComponent } from './components/skills-concepts/skills-concepts.component';
 
 @NgModule({
-  declarations: [AboutMeComponent, CvDownloadComponent],
+  declarations: [AboutMeComponent, CvDownloadComponent, SkillsConceptsComponent],
   imports: [CommonModule, AboutMeRoutingModule, SharedModule],
 })
 export class AboutMeModule {}
---
+++ src/app/modules/about-me/components/skills-concepts/skills-concepts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-concepts',
+  templateUrl: './skills-concepts.component.html',
+  styleUrls: ['./skills-concepts.component.css']
+})
+export class SkillsConceptsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
