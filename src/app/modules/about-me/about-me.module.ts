07aac8633d7db3fa0ca549b5e3889d136ca80471
GeraldSalazar <geraldsalazar4@gmail.com>
2022-10-08 10:49:12 -0600

    added cv download section to about me page

--- src/app/modules/about-me/about-me.module.ts
+++ src/app/modules/about-me/about-me.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AboutMeRoutingModule } from './about-me-routing.module';
 import { AboutMeComponent } from './about-me.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CvDownloadComponent } from './components/cv-download/cv-download.component';
 
 @NgModule({
-  declarations: [AboutMeComponent],
+  declarations: [AboutMeComponent, CvDownloadComponent],
   imports: [CommonModule, AboutMeRoutingModule, SharedModule],
 })
 export class AboutMeModule {}
