ff59063030e8893155f81ec92b5e857c5e39da9b
GeraldSalazar <geraldsalazar4@gmail.com>
2022-10-10 10:56:38 -0600

    nav active bar

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  Renderer2,
+  ViewChildren,
+} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CurrentTabService } from './current-tab.service';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css'],
 })
-export class NavBarComponent implements OnInit {
-  constructor(private breakPointsObs: BreakpointObserver) {}
+export class NavBarComponent implements OnInit, AfterViewInit {
   shiftMenu: boolean = false; //To show the mobile menu and hide nav
   showMobileMenu: boolean = false; //To show the nav menu when mobile view
-
+  listenFunc: any;
+  @ViewChildren('navItem')
+  navItems!: QueryList<ElementRef<HTMLAnchorElement>>;
+  constructor(
+    private breakPointsObs: BreakpointObserver,
+    private renderer: Renderer2,
+    private currentTab: CurrentTabService,
+    private router: Router
+  ) {}
   ngOnInit() {
     this.checkMenuBreakPoint();
+  }
+  ngAfterViewInit(): void {
+    const index = this.currentTab.getCurrentTab();
+    this.renderer.addClass(this.navItems.get(index)?.nativeElement, 'active');
+
+    this.navItems.forEach((item, index) => {
+      this.listenFunc = this.renderer.listen(
+        item.nativeElement,
+        'click',
+        event => {
+          event.preventDefault();
+          let target = event.target || event.srcElement || event.currentTarget;
+          if (target) {
+            this.currentTab.setCurrentTab(index);
+            this.router.navigate([target.dataset.route]);
+          }
+        }
+      );
+    });
   }
 
   toggleBarsMenu() {
