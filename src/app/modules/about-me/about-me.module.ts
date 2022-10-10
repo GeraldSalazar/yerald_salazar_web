8ae06575ad74aabf5a00ae78bab5849b020e0781
GeraldSalazar <geraldsalazar4@gmail.com>
2022-10-09 23:08:14 -0600

    experience section

--- src/app/modules/about-me/about-me.module.ts
+++ src/app/modules/about-me/about-me.module.ts
@@ -6,9 +6,10 @@ import { AboutMeComponent } from './about-me.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CvDownloadComponent } from './components/cv-download/cv-download.component';
 import { SkillsConceptsComponent } from './components/skills-concepts/skills-concepts.component';
+import { ExperienceItemComponent } from './components/experience-item/experience-item.component';
 
 @NgModule({
-  declarations: [AboutMeComponent, CvDownloadComponent, SkillsConceptsComponent],
+  declarations: [AboutMeComponent, CvDownloadComponent, SkillsConceptsComponent, ExperienceItemComponent],
   imports: [CommonModule, AboutMeRoutingModule, SharedModule],
 })
 export class AboutMeModule {}
