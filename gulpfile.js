912af51ac591ede6f852b9bd2d11fb827c307477
GeraldSalazar <geraldsalazar4@gmail.com>
2022-10-11 11:09:13 -0600

    image optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-/* eslint-env es6 */
-/* eslint-disable no-console */
-
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-
-gulp.task('default', () => {
-    gulp.src('src/assets/images/**/*')
-   .pipe(imagemin())
-   .pipe(gulp.dest('src/assets/imagesOptimized'))
-});
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+import pngquant from 'imagemin-pngquant';
+export default () => (
+	gulp.src('src/assets/images/**/*')
+		.pipe(imagemin([
+            pngquant({quality: [0.5, 0.75]})
+          ]))
+		.pipe(gulp.dest('src/assets/images/')),
+    gulp.src('src/assets/portfolio/images/*')
+		.pipe(imagemin([
+            pngquant({quality: [0.5, 0.75]})
+          ]))
+		.pipe(gulp.dest('src/assets/portfolio/images/'))
+);
